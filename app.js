ab62fdf5ad9889ab52e835a46b3d4c9e73aefe7b
aahmad94 <aka5vm@virginia.edu>
2018-02-07 20:07:19 -0800

    refactor app.js code

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
 const express = require('express');
 const app = express();
-const port = 5000;
-app.listen(port, () => {
-    console.log(`Console started on port ${port}`);
-});
-
 
 // Index Route
 
@@ -17,3 +12,9 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
     res.send('ABOUT');
 });   
+
+const port = 5000;
+
+app.listen(port, () => {
+    console.log(`Console started on port ${port}`);
+});
